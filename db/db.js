16fbdcdbff68ea9c68fc4111f8999daef7cb899b
Raju Ahammad <raju@hastechit.com>
2020-03-15 17:48:29 +0600

    Beta Release

---
+++ db/db.js
@@ -0,0 +1,28 @@
+const {Schema,model} = require('mongoose');
+
+const PollSchema = new Schema({
+     title:{
+          type: String,
+          required: true,
+          trim: true
+     },
+     description:{
+          type: String,
+          required: true,
+          trim: true
+     },
+     totalVote:{
+          type: Number,
+          default: 0
+     },
+     options:{
+          type: [{
+               name: String,
+               vote: Number
+          }]
+     }
+});
+
+const Poll = model("Poll", PollSchema);
+
+module.exports = Poll;
