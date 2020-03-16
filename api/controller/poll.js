43b61f7bc9c4ed271a3eb91c4ff30f245e58fa40
Raju Ahammad <raju@hastechit.com>
2020-03-16 18:06:52 +0600

    Update & Delete Poll

---
+++ api/controller/poll.js
@@ -0,0 +1,153 @@
+const Poll = require('../model/Poll')
+
+getCreatePoll = (req, res) => {
+     res.render('createPoll');
+}
+
+postCreatePoll = async (req, res) => {
+     let {
+          title,
+          description,
+          options
+     } = req.body;
+
+     options = options.map(opt => {
+          return {
+               name: opt,
+               vote: 0
+          }
+     });
+
+     let poll = new Poll({
+          title,
+          description,
+          options
+     });
+
+
+     try {
+          await poll.save();
+          res.redirect('/poll');
+     } catch (e) {
+          console.log(e)
+     }
+}
+
+
+getSinglePoll = async (req, res) => {
+     let id = req.params.id;
+     try {
+          let singlePoll = await Poll.findById(id);
+          let options = [...singlePoll.options];
+          let results = [];
+          options.forEach(option => {
+               let percentage = (option.vote * 100) / singlePoll.totalVote;
+
+               results.push({
+                    ...option._doc,
+                    percentage: percentage ? percentage : 0
+               });
+          })
+
+          res.render('singlePoll', {
+               singlePoll,
+               results
+          });
+     } catch (e) {
+          console.log(e)
+     }
+}
+
+postSinglePoll = async (req, res) => {
+     let id = req.params.id
+     let optionId = req.body.option
+     try {
+          let poll = await Poll.findById(id)
+          let options = [...poll.options]
+
+          let index = options.findIndex(o => o.id === optionId)
+          options[index].vote = options[index].vote + 1
+
+          let totalVote = poll.totalVote + 1
+
+          await Poll.findOneAndUpdate({
+               _id: poll._id
+          }, {
+               $set: {
+                    options,
+                    totalVote
+               }
+          })
+
+          res.redirect('/poll/' + id)
+
+     } catch (e) {
+          console.log(e)
+     }
+}
+
+getAllPoll = async (req, res) => {
+     try {
+          let polls = await Poll.find();
+          res.render('poll', {
+               polls
+          });
+     } catch (e) {
+          console.log(e)
+     }
+}
+
+deletePoll = (req, res) => {
+     let id = req.params.id;
+     try {
+          Poll.findByIdAndRemove(id).exec();
+          res.redirect('/poll')
+     } catch (e) {
+          console.log(e)
+     }
+}
+
+getUpdatePoll = async (req, res) => {
+     let id = req.params.id;
+
+     try {
+          let updateSinglePoll = await Poll.findById(id);
+
+          res.render('updatePoll', {
+               updateSinglePoll
+          });
+     } catch (err) {
+          console.log(err)
+     }
+}
+
+postUpdatePoll = async (req, res) => {
+     let id = req.params.id;
+     let {title, description, options} = req.body;
+     try {
+          await Poll.findOneAndUpdate({
+               _id: id
+          }, {
+               $set: {
+                    title,
+                    description
+               }
+          })
+
+          res.redirect('/poll/' + id)
+
+     } catch (err) {
+          console.log(err)
+     }
+}
+
+module.exports = {
+     getCreatePoll,
+     postCreatePoll,
+     getSinglePoll,
+     postSinglePoll,
+     getAllPoll,
+     deletePoll,
+     getUpdatePoll,
+     postUpdatePoll
+}
--- app.js
+++ app.js
@@ -1,115 +1,56 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 const app = express();
-const Poll = require('./db/db');
+const Poll = require('./api/model/Poll');
+const {
+     postCreatePoll,
+     getSinglePoll,
+     postSinglePoll,
+     getAllPoll,
+     deletePoll,
+     getUpdatePoll,
+     postUpdatePoll
+} = require('./api/controller/poll');
 
 app.set('view engine', 'ejs');
 
+// Middleware
+app.use(express.urlencoded({
+     extended: true
+}));
 
-app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use('/pub',express.static('public'));
 
-app.get('/poll/create',(req,res)=>{
-     res.render('createPoll');
-});
+app.use('/pub', express.static('public'));
 
-app.post('/poll/create', async (req,res)=>{   
-     let {title,description, options} = req.body;
+// All Routes
+app.get('/poll/update/:id', getUpdatePoll);
 
-     options = options.map(opt=>{
-          return {
-               name: opt,
-               vote: 0
-          }
-     });
+app.post('/poll/update/:id', postUpdatePoll);
 
-     let poll = new Poll({
-          title,
-          description,
-          options
-     });
+app.post('/poll/delete/:id', deletePoll);
 
+app.get('/poll/create', getCreatePoll);
 
-     try{
-          await poll.save();
-          res.redirect('/poll');
-     }catch (e){
-          console.log(e)
-     }
-});
+app.post('/poll/create', postCreatePoll);
 
+app.get('/poll/:id', getSinglePoll);
 
-app.get('/poll/:id', async (req,res)=>{
-     let id = req.params.id;
-     try{
-          let singlePoll = await Poll.findById(id);
-          let options = [...singlePoll.options];
-          let results = [];
-          options.forEach(option=>{
-               let percentage = (option.vote * 100) / singlePoll.totalVote;
+app.post('/poll/:id', postSinglePoll);
 
-               results.push({
-                    ...option._doc,
-                    percentage: percentage ? percentage : 0
-               });
-          })
-          
-          res.render('singlePoll', {singlePoll,results});
-     }catch(e){
-          console.log(e)
-     }
-})
+app.get('/poll', getAllPoll);
 
-app.post('/poll/:id', async (req,res)=>{
-     let id = req.params.id
-    let optionId = req.body.option
-    try {   
-        let poll = await Poll.findById(id)
-        let options = [...poll.options]
+app.get('/', getAllPoll);
 
-        let index = options.findIndex(o => o.id === optionId)
-        options[index].vote = options[index].vote + 1
-
-        let totalVote = poll.totalVote + 1
-
-        await Poll.findOneAndUpdate(
-            { _id: poll._id },
-            {$set: {options, totalVote}}
-        )
-
-        res.redirect('/poll/' + id)
-
-    } catch (e) {
-        console.log(e)
-    }
-})
-
-
-app.get('/poll', async (req,res)=>{
-     try{
-          let polls = await Poll.find();
-          res.render('poll', {polls});
-     } catch(e){
-          console.log(e)
-     }
-});
-
-
-app.get('/', async (req,res)=>{
-     try{
-          let polls = await Poll.find();
-          res.render('poll', {polls});
-     } catch(e){
-          console.log(e)
-     }
-});
-
-mongoose.connect('mongodb://localhost/poll', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
-.then(()=>{
-     app.listen(4444,()=>{
-          console.log('Application Ready with PORT 4444');
+// Connection & Listen
+mongoose.connect('mongodb://localhost/poll', {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useFindAndModify: false
      })
-})
-.catch(err=>console.log(err));
+     .then(() => {
+          app.listen(4444, () => {
+               console.log('Application Ready with PORT 4444');
+          })
+     })
+     .catch(err => console.log(err));
